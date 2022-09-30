9ccb69e269cf7deeb96cb51bae038836ae29ede2
Kuber144 <kuberjain144@gmail.com>
2022-09-30 12:07:00 +0530

    some font changes

--- app/src/main/java/com/example/testingproject/user_login.java
+++ app/src/main/java/com/example/testingproject/user_login.java
@@ -2,6 +2,7 @@ package com.example.testingproject;
 import static android.view.View.*;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,10 +10,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class user_login extends AppCompatActivity implements OnClickListener {
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        tv=findViewById(R.id.loginsign);
+        Typeface customfont=Typeface.createFromAsset(getAssets(),"fonts/Lobster-Regular.ttf");
+        tv.setTypeface(customfont);
     TextView sup=(TextView)findViewById(R.id.signupbutt);
     sup.setOnClickListener(this);
     }
--- app/src/main/java/com/example/testingproject/user_signup.java
+++ app/src/main/java/com/example/testingproject/user_signup.java
@@ -2,13 +2,27 @@ package com.example.testingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class user_signup extends AppCompatActivity {
-
+public class user_signup extends AppCompatActivity implements OnClickListener {
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_signup);
+        tv=findViewById(R.id.sigsign);
+        Typeface customfont=Typeface.createFromAsset(getAssets(),"fonts/Lobster-Regular.ttf");
+        tv.setTypeface(customfont);
+        ImageView bksn=(ImageView) findViewById(R.id.backbuttsign);
+        bksn.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        startActivity(new Intent(user_signup.this,user_login.class));
     }
 }
